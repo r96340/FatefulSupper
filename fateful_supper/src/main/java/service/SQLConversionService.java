a56578769a14e88b4d84e3f8cf5724031ceee07b
r96340 <r96340@gmail.com>
2025-09-24 11:53:53 +0800

    feat: 完成初版SQL API

--- fateful_supper/src/main/java/service/SQLConversionService.java
+++ fateful_supper/src/main/java/service/SQLConversionService.java
@@ -8,14 +8,24 @@ public class SQLConversionService {
     JSONObject responseJson;
     JSONArray places;
 
-    String name;
-    String location;
-    String address;
-    String gmap_link;
-    String open_hours;
-    Double rating;
-    String category;
-    String pricing;
+    String name = "";
+    String location = "";
+    String address = "";
+    String gmap_link = "";
+    String open_hours = "";
+    Double rating = 5.0;
+    String category = "";
+    String pricing = "";
+    String description = "";
+    String gallery = "";
+    String top_comments = "";
+    String website = "";
+    String phone = "";
+    String payment = "";
+    String takeout = "UNKNOWN";
+    int pet = 0;
+    int wc = 0;
+    String gmap_id = "";
 
     public SQLConversionService(String placesResponse) {
         this.responseJson = new JSONObject(placesResponse);
@@ -28,8 +38,9 @@ public class SQLConversionService {
             var place = places.getJSONObject(i);
             parse(place);
             sql.append(String.format(
-                "INSERT INTO shops (shop_name, location, address, gmap_link, open_hours, rating, category, pricing) VALUES ('%s', '%s', '%s', '%s', '%s', %.1f, '%s', '%s');<br>",
-                name, location, address, gmap_link, open_hours, rating, category, pricing
+                "INSERT INTO shops (shop_name, location, address, gmap_link, open_hours, rating, category, pricing, description, gallery, top_comments, website, phone, payment, takeout, pet, wc, gmap_id)"
+                + "VALUES ('%s', '%s', '%s', '%s', '%s', %.1f, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%d', '%d', '%s');<br>",
+                name, location, address, gmap_link, open_hours, rating, category, pricing, description, gallery, top_comments, website, phone, payment, takeout, pet, wc, gmap_id
             ));
         }
         return sql.toString();
@@ -50,10 +61,9 @@ public class SQLConversionService {
             open_hours = weekdayDescriptions.toString(0).replace("\\u2013", "-");
         }
         rating = place.optDouble("rating");
+        if(rating.isNaN()) rating = 5.0;
         JSONObject primaryTypeDisplayName = place.optJSONObject("primaryTypeDisplayName");
-        if(primaryTypeDisplayName != null){
-            category = primaryTypeDisplayName.optString("text");
-        }
+        if(primaryTypeDisplayName != null) category = primaryTypeDisplayName.optString("text");
         JSONObject priceRange = place.optJSONObject("priceRange");
         if(priceRange != null){
             JSONObject startPrice = priceRange.optJSONObject("startPrice");
@@ -62,14 +72,61 @@ public class SQLConversionService {
             String priceCeiling;
             if(endPrice != null){
                 priceCeiling = endPrice.optString("units");
-            } else {
-                priceCeiling = "";
-            }
+            } else priceCeiling = "";
             StringBuilder pricingBuilder = new StringBuilder("{\"floor\":\"");
             pricingBuilder.append(priceFloor).append("\",");
             pricingBuilder.append("\"ceiling\":\"").append(priceCeiling).append("\"}");
             pricing = pricingBuilder.toString();
+            
         }
+        JSONObject editorialSummary = place.optJSONObject("editorialSummary");
+        if(editorialSummary != null){
+            description = editorialSummary.optString("text");
+        } else description = "";
+        JSONArray photos = place.optJSONArray("photos");
+        if(photos != null){
+            JSONObject topPhoto = photos.getJSONObject(0);
+            JSONArray topPhotoAttributions = topPhoto.getJSONArray("authorAttributions");
+            JSONObject topPhotoAttributionItem = topPhotoAttributions.getJSONObject(0);
+            gallery = topPhotoAttributionItem.optString("photoUri");
+        }
+        JSONArray reviews = place.optJSONArray("reviews");
+        if(reviews != null){
+            JSONObject topReview = reviews.getJSONObject(0);
+            JSONObject topReviewTextWrap = topReview.getJSONObject("text");
+            top_comments = topReviewTextWrap.optString("text");
+        }
+        website = place.optString("websiteUri");
+        phone = place.optString("nationalPhoneNumber");
+        JSONObject paymentOptions = place.optJSONObject("paymentOptions");
+        if(paymentOptions != null){
+            Boolean acceptsCashOnly = paymentOptions.optBoolean("acceptsCashOnly");
+            if(acceptsCashOnly) {
+                payment = "{\"cash\":1}";
+            } else {
+                StringBuilder paymentBuilder = new StringBuilder("{\"cash\":1,");
+                Boolean creditCards = paymentOptions.optBoolean("acceptsCreditCards");
+                if(creditCards) paymentBuilder.append("\"creditCards\":1,");
+                Boolean debitCards = paymentOptions.optBoolean("acceptsDebitCards");
+                if(debitCards) paymentBuilder.append("\"debitCards\":1,");
+                Boolean mobile = paymentOptions.optBoolean("acceptsNfc");
+                if(mobile) {
+                    paymentBuilder.append("\"mobile\":1");
+                } else paymentBuilder.append("\"mobile\":0");
+                paymentBuilder.append("}");
+                payment = paymentBuilder.toString();
+            }
+        }
+        Boolean canTakeout = place.optBoolean("takeout");
+        Boolean canDineIn = place.optBoolean("dineIn");
+        if(canTakeout != null && canDineIn != null){
+            takeout = canTakeout ? "YES" : "NO";
+            takeout = canDineIn ? "YES" : "ONLY";
+        }
+        Boolean allowDogs = place.optBoolean("allowDogs");
+        if(allowDogs) pet = 1;
+        Boolean hasRestroom = place.optBoolean("restroom");
+        if(hasRestroom) wc = 1;
+        gmap_id = place.optString("id");
     }
-
 }
